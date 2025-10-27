d900e2e1a014c2a99878bc7994228ffefc1154a5
mnhthompson <mnhthompson@gmail.com>
2025-10-27 12:12:37 -0700

    revert 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET || 'secret',
   resave: false,
   saveUninitialized: false, 
-  cookie: { secure: true } 
+  cookie: { secure: false } 
 }));
 
 
@@ -23,9 +23,6 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
