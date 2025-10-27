fa8774e2de03766d69b126aadc65e5124ec53774
mnhthompson <mnhthompson@gmail.com>
2025-10-27 09:27:52 -0700

    fold

--- db/auth.js
+++ db/auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongodb = require('./db/connect');
+const mongodb = require('./connect');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
