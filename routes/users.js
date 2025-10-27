fdbef7fade00cd6e9741e464a4286b446b03b355
mnhthompson <mnhthompson@gmail.com>
2025-10-27 09:43:50 -0700

    errors

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/usersController');
+const usersController = require('../controllers/users');
 
 // GET all users
 router.get('/', usersController.getAllUsers);
